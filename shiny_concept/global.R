9cb1d323624129ad52a4a70565d4d6195ccba5cf
Victor <VictorYu@sent.com>
2020-06-19 13:39:25 +0100

    remove folder lookup

--- shiny_concept/global.R
+++ shiny_concept/global.R
@@ -5,8 +5,8 @@ library(plotly)
 
 options(scipen = 999)
 
-source("./../half_life_factor.R")
-source("./../run_model.R")
+source("half_life_factor.R")
+source("run_model.R")
 
 css_table <- "#contents {
 font-size: 10px
