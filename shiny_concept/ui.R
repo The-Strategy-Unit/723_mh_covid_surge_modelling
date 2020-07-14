f482556bf29845c15d16cd8d715e926809e60901
Tom Jemmett <tom.jemmett@gmail.com>
2020-07-14 14:29:27 +0100

    refactor UI
    * reformats code
    * removes unused UI elements

--- shiny_concept/ui.R
+++ shiny_concept/ui.R
@@ -1,164 +1,138 @@
 library(shiny)
 
-shinyUI(navbarPage(
-  title = "Covid Surge MH Modelling",
-  tabPanel(
-    "Plot",
-    tags$style(css_table),
-    sidebarPanel(
-      panel(
-        heading = "Population Groups",
-        selectInput(
-          "popn_subgroup",
-          "Choose subgroup",
-          choices = NA
-        ),
-        numericInput(
-          "totalmonths",
-          "Months in Model",
-          min = 1,
-          max = 24,
-          value = 24,
-          step = 1
-        ),
-        fluidRow(column(
-          6,
+shinyUI(
+  navbarPage(
+    "Covid Surge MH Modelling",
+    tabPanel(
+      "Plot",
+      sidebarPanel(
+        panel(
+          heading = "Population Groups",
+          selectInput(
+            "popn_subgroup",
+            "Choose subgroup",
+            choices = NA
+          ),
           numericInput(
-            "subpopulation_size",
-            "Subpopulation Figure",
-            value = NA,
-            step = 100
-          )
-        ),
-        column(
-          6,
-          numericInput(
-            "subpopulation_pcnt",
-            "% in subgroup",
-            value = 100,
-            min = 0,
-            max = 100,
+            "totalmonths",
+            "Months in Model",
+            min = 1,
+            max = 24,
+            value = 24,
             step = 1
-          )
-        )),
-        selectInput(
-          "subpopulation_curve",
-          "Choose scenario",
-          selected = NA,
-          choices = NA
-        )
-      ),
-      panel(
-        heading = "Treatments",
-        fluidRow(
-          column(
-            6,
-            selectInput(
-              "sliders_select_cond",
-              label = "Condition",
-              choices = NA
+          ),
+          fluidRow(
+            column(
+              6,
+              numericInput(
+                "subpopulation_size",
+                "Subpopulation Figure",
+                value = NA,
+                step = 100
+              )
+            ),
+            column(
+              6,
+              numericInput(
+                "subpopulation_pcnt",
+                "% in subgroup",
+                value = 100,
+                min = 0,
+                max = 100,
+                step = 1
+              )
             )
           ),
-          column(
-            6,
-            selectInput(
-              "sliders_select_treat",
-              label = "Treatment Pathway",
-              choices = NA
-            )
+          selectInput(
+            "subpopulation_curve",
+            "Choose scenario",
+            selected = NA,
+            choices = NA
           )
         ),
-        paste0(
-          "Idea here is to have the same sliders for each of the treatment ",
-          "groups, but the figures will change accordingly based on the ",
-          "selected group in the dropbox above and these will modify the ",
-          "parameters/graphs"
+        panel(
+          heading = "Treatments",
+          fluidRow(
+            column(
+              6,
+              selectInput(
+                "sliders_select_cond",
+                label = "Condition",
+                choices = NA
+              )
+            ),
+            column(
+              6,
+              selectInput(
+                "sliders_select_treat",
+                label = "Treatment Pathway",
+                choices = NA
+              )
+            )
+          ),
+          paste0(
+            "Idea here is to have the same sliders for each of the treatment ",
+            "groups, but the figures will change accordingly based on the ",
+            "selected group in the dropbox above and these will modify the ",
+            "parameters/graphs"
+          ),
+          sliderInput(
+            "slider_pcnt",
+            "Prevalence in sub-population",
+            min = 0,
+            max = 100,
+            value = 0,
+            step = 0.01,
+            post = "%"
+          ),
+          sliderInput(
+            "slider_treat",
+            "% Requiring Treatment",
+            min = 0,
+            max = 100,
+            value = 0,
+            step = 0.01,
+            post = "%"
+          ),
+          sliderInput(
+            "slider_success",
+            "Success % of Treatment",
+            min = 0,
+            max = 100,
+            value = 0,
+            step = 0.01,
+            post = "%"
+          )
         ),
-        sliderInput(
-          "slider_pcnt",
-          "Prevalence in sub-population",
-          min = 0,
-          max = 100,
-          value = 0,
-          step = 0.01,
-          post = "%"
+        panel(
+          heading = "Demand",
+          selectInput(
+            "treatment_type",
+            "Treatment type",
+            choices = NA
+          ),
+          sliderInput(
+            "treatment_appointments",
+            "Average demand per person",
+            min = 0,
+            max = 10,
+            step = .01,
+            value = 0
+          )
         ),
-        sliderInput(
-          "slider_treat",
-          "% Requiring Treatment",
-          min = 0,
-          max = 100,
-          value = 0,
-          step = 0.01,
-          post = "%"
-        ),
-        sliderInput(
-          "slider_success",
-          "Success % of Treatment",
-          min = 0,
-          max = 100,
-          value = 0,
-          step = 0.01,
-          post = "%"
-        )
+        downloadButton("download_params", "Download current parameters")
       ),
-      panel(
-        heading = "Demand",
+      mainPanel(
         selectInput(
-          "treatment_type",
-          "Treatment type",
-          choices = NA
+          "popn_subgroup_plot",
+          "Choose subgroups to plot",
+          choices = NA,
+          multiple = T
         ),
-        sliderInput(
-          "treatment_appointments",
-          "Average demand per person",
-          min = 0,
-          max = 10,
-          step = .01,
-          value = 0
-        )
-      ),
-      downloadButton("download_params", "Download current parameters")
-    ),
-    mainPanel(
-      selectInput(
-        "popn_subgroup_plot",
-        "Choose subgroups to plot",
-        choices = NA,
-        multiple = T
-      ),
-      withSpinner(plotlyOutput("pop_plot")),
-      withSpinner(plotlyOutput("demand_plot")),
-      downloadButton("download_output", "Download model output")
-    )
-  ),
-  tabPanel(
-    "Example Distribution",
-    verbatimTextOutput("unemployed_y_vec"),
-    verbatimTextOutput("bereaved_y_vec"),
-    verbatimTextOutput("o_print_test"),
-    verbatimTextOutput("conditions_test")
-  ),
-  tabPanel(
-    "Demand",
-    sidebarPanel(
-      selectInput(
-        "demand_treatment_type",
-        "Treatment type",
-        choices = NA
-      ),
-      sliderInput(
-        "demand_treatment_demand",
-        "Average demand per person",
-        min = 0,
-        max = 10,
-        step = .01,
-        value = 0
+        withSpinner(plotlyOutput("pop_plot")),
+        withSpinner(plotlyOutput("demand_plot")),
+        downloadButton("download_output", "Download model output")
       )
-    ),
-    mainPanel(
-      plotlyOutput("demand_demand_plot"),
-      tableOutput("demand_table")
     )
   )
-))
+)
