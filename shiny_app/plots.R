054cb6eba717684718329e3dd4ea9f10b188b758
Tom Jemmett <tom.jemmett@gmail.com>
2020-07-28 12:15:44 +0100

    adds surge_plot function

--- shiny_app/plots.R
+++ shiny_app/plots.R
@@ -26,3 +26,28 @@ demand_plot <- function(demand) {
                    xaxis = list(title = "Month"),
                    yaxis = list(title = "Demand"))
 }
+
+
+surge_plot <- function(data) {
+  data %>%
+    mutate_at("new-referral", `-`, quo(`new-treatment`)) %>%
+    rename("Received treatment" = `new-treatment`,
+           "Referred, but not treated" = `new-referral`) %>%
+    pivot_longer(-group) %>%
+    mutate_at("name", fct_rev) %>%
+    ggplot(aes(value, group, fill = name)) +
+    geom_col() +
+    scale_x_continuous(expand = expansion(c(0, 0.05))) +
+    scale_fill_discrete(guide = guide_legend(reverse = TRUE)) +
+    theme(panel.background = element_blank(),
+          panel.grid = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.line.x = element_line(),
+          legend.position = c(1, 0),
+          legend.justification = c(1, 0),
+          legend.title = element_blank()) +
+    labs(y = "",
+         x = "Total Referrals / Treatments",
+         caption = paste0("The total receiving services offered for each condition will never exceed the total",
+                          "symptomatic over period referrals"))
+}
