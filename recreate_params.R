e18f338576e3183862684b215c7f9bfb7d5a2073
Tom Jemmett <tom.jemmett@gmail.com>
2020-08-04 17:40:23 +0100

    functionalises recreate_params.R code

--- recreate_params.R
+++ recreate_params.R
@@ -1,35 +1,34 @@
-library(writexl)
+params_to_xlsx <- function(params) {
+  xl <- list()
 
-# reconstruct xlsx
+  xl$curves <- params$curves %>%
+    bind_cols() %>%
+    mutate(month = row_number()-1, .before = everything())
 
-xl <- list()
+  xl$groups <- params$groups %>%
+    map_dfr(modify_at, "conditions", ~NULL, .id = "group") %>%
+    select(group, curve, size, pcnt)
 
-xl$curves <- params$curves %>%
-  bind_cols() %>%
-  mutate(month = row_number()-1, .before = everything())
-
-xl$groups <- params$groups %>%
-  map_dfr(modify_at, "conditions", ~NULL, .id = "group") %>%
-  select(group, curve, size, pcnt)
-
-xl$g2c <- params$groups %>%
-  map("conditions") %>%
-  map(map_dbl, "pcnt") %>%
-  map_dfr(enframe, name = "condition", value = "pcnt", .id = "group")
+  xl$g2c <- params$groups %>%
+    map("conditions") %>%
+    map(map_dbl, "pcnt") %>%
+    map_dfr(enframe, name = "condition", value = "pcnt", .id = "group")
 
 
-xl$c2t <- params$groups %>%
-  map("conditions") %>%
-  map_depth(2, "treatments") %>%
-  map_depth(3, bind_cols) %>%
-  map_depth(2, bind_rows, .id = "treatment") %>%
-  map(bind_rows, .id = "condition") %>%
-  bind_rows(.id = "group")
+  xl$c2t <- params$groups %>%
+    map("conditions") %>%
+    map_depth(2, "treatments") %>%
+    map_depth(3, bind_cols) %>%
+    map_depth(2, bind_rows, .id = "treatment") %>%
+    map(bind_rows, .id = "condition") %>%
+    bind_rows(.id = "group")
 
-xl$treatments <- params$treatments %>%
-  map_dfr(bind_cols, .id = "treatment")
+  xl$treatments <- params$treatments %>%
+    map_dfr(bind_cols, .id = "treatment")
 
-xl$demand <- params$demand %>%
-  bind_rows(.id = "service")
+  xl$demand <- params$demand %>%
+    bind_rows(.id = "service")
 
-write_xlsx(xl, "params.xlsx")
+  write_xlsx(xl, "params.xlsx")
+}
+
