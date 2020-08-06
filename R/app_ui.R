01a71dc098e0d14fa7abcb222f878e8fa985ed83
Tom Jemmett <tom.jemmett@gmail.com>
2020-08-06 17:53:41 +0100

    fixes issue with over-zealous search and replace

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,13 +1,13 @@
-#" The application User-Interface
-#"
-#" @param request Internal parameter for `{shiny}`.
-#"     DO NOT REMOVE.
-#" @import shiny
-#" @import shinydashboard
-#" @import shinycssloaders
-#" @import shinyWidgets
-#" @importFrom plotly plotlyOutput
-#" @noRd
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import shinydashboard
+#' @import shinycssloaders
+#' @import shinyWidgets
+#' @importFrom plotly plotlyOutput
+#' @noRd
 app_ui <- function(request) {
 
   params_population_groups <- box(
@@ -325,14 +325,14 @@ app_ui <- function(request) {
   )
 }
 
-#" Add external Resources to the Application
-#"
-#" This function is internally used to add external
-#" resources inside the Shiny application.
-#"
-#" @import shiny
-#" @importFrom golem add_resource_path activate_js favicon bundle_resources
-#" @noRd
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
 golem_add_external_resources <- function() {
 
   add_resource_path(
