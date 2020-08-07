39b2fdc0997da865d1ba3f4cb198ca237730d955
Tom Jemmett <thomas.jemmett@nhs.net>
2020-08-07 16:09:53 +0100

    Fix ci cmd check issue (#73)
    * fixes issue with test not running properly in R cmd check by skipping it

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -13,23 +13,42 @@ test_that("app server", {
 })
 
 # Configure this test to fit your need
-test_that(
-  "app launches", {
-    skip_on_cran()
-    skip_on_travis()
-    skip_on_appveyor()
-    x <- processx::process$new(
-      "R",
-      c(
-        "-e",
-        "pkgload::load_all(here::here());run_app()"
-      )
-    )
-    i <- 0
-    while ((i <- i + 1) <= 30 && !x$is_alive()) {
-      Sys.sleep(1)
-    }
-    expect_true(x$is_alive())
-    x$kill()
+test_that("app launches", {
+  if(!"R" %in% dir(here::here())) {
+    skip("This test only works in devtools::test")
   }
-)
+
+  # The issue is around here::here(), it will be running in
+  # golemTest.Rcheck/tests/testthat
+  # on R cmd check, so it won't find the functions to load
+
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+  skip_on_ci()
+  x <- processx::process$new(
+    "R",
+    c(
+      "-e",
+      "pkgload::load_all(here::here());run_app()"
+    )
+  )
+  Sys.sleep(5)
+  expect_true(x$is_alive())
+  x$kill()
+})
+
+test_that("run_app starts the application correctly", {
+  m <- mock("app")
+
+  with_mock(with_golem_options = m, {
+    res <- run_app()
+  })
+
+  expect_called(m, 1)
+  expect_call(m, 1, with_golem_options(
+    app = shinyApp(ui = app_ui, server = app_server),
+    golem_opts = list(...)
+  ))
+  expect_equal(res, "app")
+})
