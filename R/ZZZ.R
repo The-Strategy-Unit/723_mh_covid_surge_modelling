c7861a09aef4777ccc08bf020b0fd92360009738
Tom Jemmett <tom.jemmett@gmail.com>
2020-08-07 17:06:17 +0100

    moves app intialisation code to ZZZ.R's .onLoad function

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -1,1 +1,28 @@
 utils::globalVariables("group")
+
+#' @importFrom magrittr %>%
+#' @importFrom jsonlite read_json
+#' @importFrom purrr modify_at map
+.onLoad <- function(libname, pkgname) {
+  sim_time <<- as.numeric(Sys.getenv("SIM_TIME", 1 / 5))
+
+  params <<- app_sys("app/data/params.json") %>%
+    read_json(simplifyVector = TRUE) %>%
+    modify_at("demand", as.list)
+
+  population_groups <<- names(params$groups)
+
+  treatments <<- names(params$treatments)
+
+  models <<- local({
+    models_file <- app_sys("app/data/models.Rds")
+    if (!file.exists(models_file)) {
+      params$groups %>%
+        names() %>%
+        set_names() %>%
+        map(~run_single_model(params, .x, 24, sim_time)) %>%
+        saveRDS(models_file)
+    }
+    readRDS(models_file)
+  })
+}
--- R/app_server.R
+++ R/app_server.R
@@ -15,29 +15,6 @@
 #' @importFrom utils write.csv
 #' @noRd
 app_server <- function(input, output, session) {
-  # app init ----
-  sim_time <- as.numeric(Sys.getenv("SIM_TIME", 1 / 5))
-
-  params <- app_sys("app/data/params.json") %>%
-    read_json(simplifyVector = TRUE) %>%
-    modify_at("demand", as.list)
-
-  population_groups <- names(params$groups)
-
-  treatments <- names(params$treatments)
-
-  models <- local({
-    models_file <- app_sys("app/data/models.Rds")
-    if (!file.exists(models_file)) {
-      params$groups %>%
-        names() %>%
-        set_names() %>%
-        map(~run_single_model(params, .x, 24, sim_time)) %>%
-        saveRDS(models_file)
-    }
-    readRDS(models_file)
-  })
-  # done initialising ----
 
   population_groups <- reactiveVal(population_groups)
   all_conditions <- reactiveVal(get_all_conditions(params))
