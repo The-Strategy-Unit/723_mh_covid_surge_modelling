d2b5ad71dbec3094d506134ce682e7c6f595d771
Tom Jemmett <tom.jemmett@gmail.com>
2020-09-11 22:45:46 +0100

    makes summarise_model_output work with multiple treatments

--- R/fct_plot_helpers.R
+++ R/fct_plot_helpers.R
@@ -4,20 +4,16 @@
 #'
 #' @param model_output output from \code{run_model()} and \code{get_model_output()}
 #' @param type the row "type" to filter by
-#' @param treatment a name of a treatment to filter by
+#' @param treatments the list of treatments to filter model_output by
 #'
 #' @return a filtered and summarised version of \code{model_output}
 #'
 #' @importFrom dplyr %>% filter group_by summarise across
 #' @import rlang
-summarise_model_output <- function(model_output, type, treatment) {
-  # "treatment" is usually passed in as a variable, this value needs to be forced, otherwise treatment will just remain
-  # as a call
-  force(treatment)
-
+summarise_model_output <- function(model_output, type, treatments) {
   model_output %>%
     filter(.data$type == {{type}},
-           .data$treatment == {{treatment}}) %>%
+           .data$treatment %in% treatments) %>%
     group_by(.data$date, .add = TRUE) %>%
     summarise(across(.data$value, sum), .groups = "drop_last")
 }
