b18a3ab3e84a236eab76e567f84d2df9345041f4
Tom Jemmett <tom.jemmett@gmail.com>
2020-09-15 10:44:22 +0100

    uncomments line from run_dev.R to clear environment

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -3,7 +3,7 @@ options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mo
 
 # Detach all loaded packages and clean your environment
 golem::detach_all_attached()
-# rm(list=ls(all.names = TRUE))
+rm(list=ls(all.names = TRUE))
 
 # Document and reload your package
 golem::document_and_reload()
