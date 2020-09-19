ac83bbc3f2068b6f5bfb10d2dc71672d296e2c0a
Tom Jemmett <tom.jemmett@gmail.com>
2020-09-19 20:06:59 +0100

    moves Summarise model output into utils_model_output_helpers.R

--- R/utils_model_output_helpers.R
+++ R/utils_model_output_helpers.R
@@ -115,3 +115,23 @@ surge_table <- function(model_output, column, column_name) {
 
   df
 }
+
+#' Summarise model output
+#'
+#' Helper function used to filter results of the model output for use in plots and tables
+#'
+#' @param model_output output from \code{run_model()} and \code{get_model_output()}
+#' @param type the row "type" to filter by
+#' @param treatments the list of treatments to filter model_output by
+#'
+#' @return a filtered and summarised version of \code{model_output}
+#'
+#' @importFrom dplyr %>% filter group_by summarise across
+#' @import rlang
+summarise_model_output <- function(model_output, type, treatments) {
+  model_output %>%
+    filter(.data$type == {{type}},
+           .data$treatment %in% treatments) %>%
+    group_by(.data$date, .add = TRUE) %>%
+    summarise(across(.data$value, sum), .groups = "drop_last")
+}
