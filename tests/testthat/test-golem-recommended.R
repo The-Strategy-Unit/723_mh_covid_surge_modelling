43864207d3c4f8c8a1c88d0b68495a0a84a49d42
Tom Jemmett <tom.jemmett@gmail.com>
2020-11-02 13:41:13 +0000

    moves run_app test to it's own file

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -1,6 +1,6 @@
-context("golem tests")
-
+library(testthat)
 library(golem)
+library(mockery)
 
 test_that("app ui", {
   ui <- app_ui()
@@ -9,10 +9,9 @@ test_that("app ui", {
 
 test_that("app server", {
   server <- app_server
-  expect_is(server, "function")
+  expect_type(server, "closure")
 })
 
-# Configure this test to fit your need
 test_that("app launches", {
   if (!"R" %in% dir(here::here())) {
     skip("This test only works in devtools::test")
@@ -32,22 +31,10 @@ test_that("app launches", {
       "pkgload::load_all(here::here());run_app()"
     )
   )
-  Sys.sleep(5)
+  for (i in 1:100) {
+    if (x$is_alive()) break()
+    Sys.sleep(0.1)
+  }
   expect_true(x$is_alive())
   x$kill()
-})
-
-test_that("run_app starts the application correctly", {
-  m <- mock("app")
-
-  with_mock(with_golem_options = m, {
-    res <- run_app()
-  })
-
-  expect_called(m, 1)
-  expect_call(m, 1, with_golem_options(
-    app = shinyApp(ui = app_ui, server = app_server),
-    golem_opts = list(...)
-  ))
-  expect_equal(res, "app")
 })
