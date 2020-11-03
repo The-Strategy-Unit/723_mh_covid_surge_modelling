82174ab001bc4e938416540d69b630743c4b233f
Tom Jemmett <tom.jemmett@gmail.com>
2020-11-03 22:46:31 +0000

    removes golem scripts that aren't being used

--- R/app_ui.R
+++ R/app_ui.R
@@ -92,7 +92,6 @@ app_ui <- function(request) {
 #' resources inside the Shiny application.
 #'
 #' @import shiny
-#' @importFrom htmltools tags
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function() {
