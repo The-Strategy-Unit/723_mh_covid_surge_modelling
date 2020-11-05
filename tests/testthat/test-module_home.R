a4604492a4a259c9c06a3fd11dcfb109cfd9a5bf
Tom Jemmett <tom.jemmett@gmail.com>
2020-11-05 20:11:01 +0000

    simplifies test stub

--- tests/testthat/test-module_home.R
+++ tests/testthat/test-module_home.R
@@ -4,7 +4,7 @@ library(mockery)
 # ui ----
 
 test_that("it generates the UI correctly", {
-  stub(home_ui, "dir", function(...) c("params_a.xlsx", "params_b.xlsx", "params_c.xlsx"))
+  stub(home_ui, "dir", c("params_a.xlsx", "params_b.xlsx", "params_c.xlsx"))
   ui <- home_ui("a")
   expect_snapshot(ui)
   expect_s3_class(ui, "shiny.tag.list")
