798a48dcd7bdd6d0142173a9bbe426fef2f6bb3d
Tom Jemmett <tom.jemmett@gmail.com>
2020-12-10 20:13:33 +0000

    reimplements @victornautica 's changes on top of master:HEAD

--- R/plot_treatment_split.R
+++ R/plot_treatment_split.R
@@ -6,6 +6,7 @@
 #'
 #' @importFrom dplyr tibble mutate across arrange desc
 #' @importFrom plotly plot_ly layout config
+#' @importFrom stringr str_wrap str_replace_all
 #' @import rlang
 #'
 #' @return a plotly chart
@@ -15,15 +16,26 @@ treatment_split_plot <- function(treatments) {
   tibble(treatment = names(treatments),
          split = treatments) %>%
     mutate(across(.data$split, ~ .x / sum(.x)),
+           across(.data$treatment, ~ .x %>%
+                    str_wrap(width = 27) %>%
+                    str_replace_all("\\n", "<br>")),
            across(.data$treatment, fct_reorder, split)) %>%
     arrange(desc(.data$split)) %>%
-    plot_ly(x = ~split,
-            y = ~treatment,
-            marker = list(color = "#586FC1",
-                          line = list(color = "#2c2825", width = 1.5)),
-            type =  "bar") %>%
-    layout(xaxis = list(tickformat = "%",
-                        title = FALSE),
-           yaxis = list(title = FALSE))  %>%
+    plot_ly(
+      x = ~ split,
+      y = ~ treatment,
+      marker = list(
+        color = "#586FC1",
+        line = list(color = "#2c2825", width = 1.5)
+      ),
+      type =  "bar"
+    ) %>%
+    layout(
+      xaxis = list(tickformat = "%",
+                   title = FALSE),
+      yaxis = list(title = FALSE,
+                   tickfont = list(size = 10)),
+      margin = list(l = 150)
+    ) %>%
     config(displayModeBar = FALSE)
 }
