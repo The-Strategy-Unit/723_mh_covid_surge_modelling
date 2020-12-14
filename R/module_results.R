1f5ead276c37d1bcd4f8073549fb667f148628cb
Victor <VictorYu@sent.com>
2020-12-14 15:05:50 +0000

    just remembered you already had code for generating date/time for the .csv output download, just replacing the code with yours for consistency though functionally the same

--- R/module_results.R
+++ R/module_results.R
@@ -134,9 +134,9 @@ results_server <- function(id, params, model_output) {
   moduleServer(id, function(input, output, session) {
     output$download_report <- downloadHandler(
       filename = if (input$download_choice == "all") {
-        paste0(Sys.Date(), "_", gsub(":", "", format(Sys.time(), "%X")), "_AllServices.pdf")
+        paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), "_AllServices.pdf")
       } else {
-        paste0(Sys.Date(), "_", gsub(":", "", format(Sys.time(), "%X")), "_", gsub(" ", "", input$services, fixed = TRUE), ".pdf")
+        paste0(format(Sys.time(), "%Y-%m-%d_%H%M%S"), "_", gsub(" ", "", input$services, fixed = TRUE), ".pdf")
       },
       content = function(file) {
         model_output <- model_output()
